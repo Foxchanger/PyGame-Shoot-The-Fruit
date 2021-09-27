8e65a0003d38f65488aef55b104f5386ab1a9f6c
Josimar Goncalves <foxchanger@hotmail.com>
2021-09-27 17:56:07 -0300

    Upd

--- Code.py
+++ Code.py
@@ -1,1 +1,23 @@
-print ("Ola mundo")
+import pgzrun
+import pygame
+from random import randint
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.draw()
+
+def place_apple ():
+    apple.x = randint(10,800)
+    apple.y = randint (10,600)
+
+def on_mousedown (pos):
+    if apple.colliderpoint(pos):
+        print("Good Shot !")
+        place_apple()
+    else:
+         print("You missed !")
+
+place_apple()
+
+pgzrun.go()
